476a4eada45d227f0dfb1c96185950c81df28a0d
Celina Sørtveit <celinasortveit@celinas-macbook-pro.home>
2021-02-12 11:38:34 +0100

    added a button

--- sellpoint_frontend/src/App.tsx
+++ sellpoint_frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import React, { FunctionComponent } from "react";
+import { Button } from "react-bootstrap";
 import "./App.css";
+
 
 const App: FunctionComponent = () => {
   return (
-    <div>
-      <h1>SellPoint</h1>
-      <p>Velkommen!</p>
+    <div className ="form-group"> 
+        <button className="btn btn-success btn-lg float-right" type="submit"> 
+           Logg Inn 
+      </button> 
     </div>
+
   );
 };
+
 
 export default App;
