e3e46bd4f59582931ec153f8e493ef7048d8470c
Eirik Lorgen Tanberg <eiritan@stud.ntnu.no>
2021-02-13 18:05:21 +0100

    Mark where we should load self user data in session context (#2)

--- sellpoint_frontend/src/context/Session.tsx
+++ sellpoint_frontend/src/context/Session.tsx
@@ -1,10 +1,19 @@
 import Session from "../models/session";
 import User from "../models/user";
 import React, { FunctionComponent, createContext, useContext, useState, useEffect } from "react";
+import AuthenticationService from "../core/auth";
 
 export const useProviderValue = (): Session => {
   const [user, setUser] = useState<User | undefined>(undefined);
   const [redirectPath, setRedirectPath] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    if (!AuthenticationService.isLoggedIn()) {
+      return;
+    }
+
+    // TODO: Pull own user from API and update user state
+  })
 
   const isAuthenticated = !!user;
   return { isAuthenticated, user, redirectPath, setUser, setRedirectPath };
