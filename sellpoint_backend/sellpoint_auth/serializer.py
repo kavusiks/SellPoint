0ce1d079d01c976bbbe8b9aeb334c07ec547b1ca
Eirik Lorgen Tanberg <eiritan@stud.ntnu.no>
2021-02-17 15:28:28 +0100

    Add endpoint auth/self/ for viewing own user (#2)

--- sellpoint_backend/sellpoint_auth/serializer.py
+++ sellpoint_backend/sellpoint_auth/serializer.py
@@ -24,4 +24,4 @@ class RegisterSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id','email','first_name', 'last_name')
+        fields = ('id','email','first_name', 'last_name', 'phone_number', 'last_login')
--- sellpoint_backend/sellpoint_auth/views.py
+++ sellpoint_backend/sellpoint_auth/views.py
@@ -2,3 +2,14 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
+from rest_framework.response import Response
+from .serializer import RegisterSerializer, UserSerializer
+from django.contrib.auth.models import User
+
+class SelfAPIView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args,  **kwargs):
+        user_self = UserSerializer(request.user, read_only=True)
+        return Response(user_self.data)
