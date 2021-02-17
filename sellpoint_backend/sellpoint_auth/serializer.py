9614f726ebcc6e14c44a80e8a13453f20b2ac3a4
Eirik Lorgen Tanberg <eiritan@stud.ntnu.no>
2021-02-17 16:31:38 +0100

    Add support for address field in serializer (#2)

--- sellpoint_backend/sellpoint_auth/serializer.py
+++ sellpoint_backend/sellpoint_auth/serializer.py
@@ -1,27 +1,41 @@
-from rest_framework import  serializers
+from rest_framework import serializers
 from rest_framework.permissions import IsAuthenticated
 from django.db import models
-from sellpoint_auth.models import User
+from sellpoint_auth.models import User, Address
 from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import make_password
 
-# Register serializer
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = ('line1', 'line2', 'postalcode', 'city', 'country')
+
+
 class RegisterSerializer(serializers.ModelSerializer):
+    address = AddressSerializer()
+
     class Meta:
         model = User
-        fields = ('id','email','password','first_name', 'last_name')
+        fields = ('id', 'email', 'password',
+                  'first_name', 'last_name', 'address')
         extra_kwargs = {
-            'password':{'write_only': True},
+            'password': {'write_only': True},
         }
+
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['email'],
-        password = validated_data['password'],
-        first_name=validated_data['first_name'],
-        last_name=validated_data['last_name'])
+        user = User.objects.create(email=validated_data['email'],
+                                   password=validated_data['password'],
+                                   first_name=validated_data['first_name'],
+                                   last_name=validated_data['last_name'])
+        Address.objects.create(user=user, **validated_data['address'])
         return user
 
-# User serializer
+
 class UserSerializer(serializers.ModelSerializer):
+    address = AddressSerializer()
+
     class Meta:
         model = User
-        fields = ('id','email','first_name', 'last_name', 'phone_number', 'last_login')
+        fields = ('id', 'email', 'first_name', 'last_name',
+                  'phone_number', 'last_login', 'address')
