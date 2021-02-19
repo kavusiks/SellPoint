4bba3886d27aa61539cde764f423b6ad23b577d5
Eirik Lorgen Tanberg <eiritan@stud.ntnu.no>
2021-02-19 21:33:43 +0100

    Remove default App.test.tsx (#2)

--- sellpoint_frontend/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders title", () => {
-  render(<App />);
-  const titleElement = screen.getByText(/SellPoint/i);
-  expect(titleElement).toBeInTheDocument();
-});
