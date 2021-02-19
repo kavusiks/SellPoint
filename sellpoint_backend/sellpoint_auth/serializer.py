f61d0ebea6f198a4b7033a4dfc67d98286286ec6
Eirik Lorgen Tanberg <eiritan@stud.ntnu.no>
2021-02-19 17:34:59 +0100

    Add password validation to RegisterSerializer (#2)

--- sellpoint_backend/sellpoint_auth/serializer.py
+++ sellpoint_backend/sellpoint_auth/serializer.py
@@ -4,6 +4,8 @@ from django.db import models
 from sellpoint_auth.models import User, Address
 from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import make_password
+from django.core import exceptions
+import django.contrib.auth.password_validation as validators
 
 
 class AddressSerializer(serializers.ModelSerializer):
@@ -39,6 +41,32 @@ class RegisterSerializer(serializers.ModelSerializer):
                                         phone_number=validated_data['phone_number'])
         Address.objects.create(user=user, **validated_data['address'])
         return user
+
+    def validate(self, validated_data):
+        # here data has all the fields which have validated values
+        # so we can create a User instance out of it
+        user = User(validated_data['email'],
+                    password=validated_data['password'],
+                    first_name=validated_data['first_name'],
+                    last_name=validated_data['last_name'],
+                    phone_number=validated_data['phone_number'])
+
+        # get the password from the data
+        password = validated_data.get('password')
+
+        errors = dict()
+        try:
+            # validate the password and catch the exception
+            validators.validate_password(password=password, user=User)
+
+        # the exception raised here is different than serializers.ValidationError
+        except exceptions.ValidationError as e:
+            errors['password'] = list(e.messages)
+
+        if errors:
+            raise serializers.ValidationError(errors)
+
+        return super(RegisterSerializer, self).validate(validated_data)
 
 
 class UserSerializer(serializers.ModelSerializer):
