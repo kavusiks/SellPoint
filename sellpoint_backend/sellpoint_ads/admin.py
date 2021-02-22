a97eee73c132b67005117236a12b88b6ae300e8f
Eirik Lorgen Tanberg <eiritan@stud.ntnu.no>
2021-02-22 16:55:10 +0100

    Update ad models and admin interface (#3)

--- sellpoint_backend/sellpoint_ads/admin.py
+++ sellpoint_backend/sellpoint_ads/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import Ad, Category
-# Register your models here.
+from .models import Ad, Category, Image
+
+class ImageInline(admin.StackedInline):
+    model = Image
+
+class AdAdmin(admin.ModelAdmin):
+    model = Ad
+    inlines = (ImageInline, )
+
 
 admin.site.register(Category)
-admin.site.register(Ad)
+admin.site.register(Ad, AdAdmin)
--- sellpoint_backend/sellpoint_ads/models.py
+++ sellpoint_backend/sellpoint_ads/models.py
@@ -1,36 +1,37 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
+from sellpoint_auth.models import User
 import os
-#from sellpoint_auth import User
+
 
 def get_image_path(instance, filename):
-    return os.path.join('photos', str(instance.id), filename)
+    return os.path.join('photos', str(instance.ad.id), filename)
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=200, unique=True, blank=False)
+    name = models.CharField(max_length=64, unique=True, blank=False)
 
     def __str__(self):
         return self.name
 
+
 class Ad(models.Model):
-    title = models.CharField(max_length=30, blank=False)
-    description = models.TextField(max_length=350)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, blank=False)
+    description = models.TextField(max_length=512)
     price = models.PositiveSmallIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-    is_sold = models.BooleanField(_('Is sold'), default=False)
-    #location =
-    #owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    img = models.ImageField(_("Image for one ad"), null=True, blank=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True)
+    is_sold = models.BooleanField(default=False)
+    thumbnail = models.ForeignKey('Image', on_delete=models.RESTRICT, related_name="thumbnail_for", null=True, blank=True)
 
 
-#Må finne ut av hvordan man bruker dette
 class Image(models.Model):
-    image = models.ImageField(_("Image for one ad"), upload_to=get_image_path, height_field=None, width_field=None, max_length=None, null=True, blank=True)
-    ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
-
-
-
-
+    image = models.ImageField(upload_to=get_image_path,
+                              height_field=None, width_field=None, max_length=None)
+    ad = models.ForeignKey(Ad, on_delete=CASCADE)
+    description = models.CharField(max_length=256, blank=True, null=True)
