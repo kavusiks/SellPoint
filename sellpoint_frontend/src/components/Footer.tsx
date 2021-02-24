d0b5af5908e62cf2ea0f0c4dbb8d77e15c385c4f
Eirik Lorgen Tanberg <eiritan@stud.ntnu.no>
2021-02-24 13:05:20 +0100

    Remove Desktop1 file and move Footer (#3)

--- sellpoint_frontend/src/components/Footer.tsx
+++ sellpoint_frontend/src/components/Footer.tsx
@@ -1,20 +1,17 @@
-import * as React from 'react'
-import { Nav, Navbar } from 'react-bootstrap';
+import React, { FunctionComponent } from "react";
+import { Nav, Navbar } from "react-bootstrap";
 
-export const Footer: React.FC = () => {
-return (
-    
+export const Footer: FunctionComponent = () => {
+  return (
     <Navbar bg="light" variant="light" fixed="bottom" className="page">
-      <Navbar.Brand className = "sellpointFont">Sellpoint </Navbar.Brand>
+      <Navbar.Brand className="sellpointFont">Sellpoint </Navbar.Brand>
       <Nav className="mr-auto">
         <Nav.Link href="#home">Sett link 1</Nav.Link>
         <Nav.Link href="#features">Sett link 2</Nav.Link>
         <Nav.Link href="#pricing">Sett link 3</Nav.Link>
       </Nav>
     </Navbar>
-)
+  );
 };
-
-
 
 export default Footer;
