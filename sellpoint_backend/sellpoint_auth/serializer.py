ff8e680ea9c724f12602ef6582cbb55373f1cb12
Eirik Lorgen Tanberg <eirik@tanberg.org>
2021-03-11 09:44:54 +0100

    Fix formatting (#23)

--- sellpoint_backend/sellpoint_auth/serializer.py
+++ sellpoint_backend/sellpoint_auth/serializer.py
@@ -90,5 +90,13 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('email', 'first_name', 'last_name',
-                  'phone_number', 'last_login', 'address', 'date_joined', 'is_staff')
+        fields = (
+            "email",
+            "first_name",
+            "last_name",
+            "phone_number",
+            "last_login",
+            "address",
+            "date_joined",
+            "is_staff",
+        )
