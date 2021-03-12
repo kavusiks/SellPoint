40ca6acc9245b28183bf94e909727e4dfa2072a8
skcheng <skcheng@stud.ntnu.no>
2021-03-12 17:04:50 +0100

    Made room for categoryselection in mainpage (#11)

--- sellpoint_frontend/src/components/MainPage.tsx
+++ sellpoint_frontend/src/components/MainPage.tsx
@@ -1,19 +1,24 @@
 import React, { FunctionComponent, useState, useEffect } from "react";
-import { Row } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import AdAPI from "../core/api/ad";
 import { Ad } from "../models/ad";
 import SmallAd from "./ads/SmallAd";
 
+const CategoryContainer = styled(Row)`
+  width: 15%;
+`;
+
 const AdContainer = styled(Row)`
+  width: 100%;
   flex-wrap: wrap;
   justify-content: center;
   align-items: center;
 `;
 
 const AdLink = styled(Link)`
-  flex: 0 30%;
+  flex: 0 47%;
   margin: 10px;
   width: 100%;
   height: 300px;
@@ -27,15 +32,23 @@ export const MainPage: FunctionComponent = () => {
   }, []);
 
   return (
-    <AdContainer noGutters>
-      {items
-        .filter((item) => !item.is_sold)
-        .map((item) => (
-          <AdLink key={item.id} to={`/ad/${item.id}`}>
-            <SmallAd ad={item} />
-          </AdLink>
-        ))}
-    </AdContainer>
+    <>
+      <Row>
+        <Col md={2}>{/* Insert selection for categories here */}</Col>
+        <Col md={8}>
+          <AdContainer noGutters>
+            {items
+              .filter((item) => !item.is_sold)
+              .map((item) => (
+                <AdLink key={item.id} to={`/ad/${item.id}`}>
+                  <SmallAd ad={item} />
+                </AdLink>
+              ))}
+          </AdContainer>
+        </Col>
+        <Col md={2}>{/* Insert commercial picture here */}</Col>
+      </Row>
+    </>
   );
 };
 
