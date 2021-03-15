276496b02ffa9e6c9ac60ca471617f315fecaf9b
skcheng <skcheng@stud.ntnu.no>
2021-03-15 14:52:50 +0100

    Added url to create favorite (#9)

--- sellpoint_backend/sellpoint_favorite/urls.py
+++ sellpoint_backend/sellpoint_favorite/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import favorite_ads_list
+from .views import favorite_ads_list, AdCreateAPIView
 
 urlpatterns = [
     path("list/", favorite_ads_list, name="favorite-ads"),
+    path("create/", AdCreateAPIView.as_view(), name="favorite-create"),
 ]
