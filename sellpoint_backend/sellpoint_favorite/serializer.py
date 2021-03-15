ef0fb286bce934eab5c055247b4d149bb92029da
skcheng <skcheng@stud.ntnu.no>
2021-03-15 14:52:34 +0100

    Added serializer for creating favorite (#9)

--- sellpoint_backend/sellpoint_favorite/serializer.py
+++ sellpoint_backend/sellpoint_favorite/serializer.py
@@ -10,3 +10,11 @@ class FavoriteAdSerializer(serializers.ModelSerializer):
     class Meta:
         model = FavoriteAd
         fields = "__all__"
+
+class FavoriteCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FavoriteAd
+        fields = ["user", "favorite_ad_id"]
+
+    def create(self, validated_data):
+        return FavoriteAd.objects.create(**validated_data)
