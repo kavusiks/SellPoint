4ee26797ddf95dc35e11300fe4a20cd7bc6bd7bb
skcheng <skcheng@stud.ntnu.no>
2021-03-15 10:14:25 +0100

    Removed unused import (#11)

--- sellpoint_frontend/src/components/forms/CreateAdForm.tsx
+++ sellpoint_frontend/src/components/forms/CreateAdForm.tsx
@@ -1,7 +1,6 @@
 import React, { FunctionComponent, useState } from "react";
 import { Button, Col, Form, Dropdown, DropdownButton } from "react-bootstrap";
 import { useHistory } from "react-router";
-import { NullLiteral } from "typescript";
 import AdAPI from "../../core/api/ad";
 import { readDjangoError } from "../../core/client";
 import { Ad } from "../../models/ad";
