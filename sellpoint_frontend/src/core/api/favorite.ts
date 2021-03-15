6bfdd39dc7e24ecb58bcbdc7c69906c0876dc4a7
skcheng <skcheng@stud.ntnu.no>
2021-03-15 14:54:05 +0100

    Create favorite in API frontend (#9)

--- sellpoint_frontend/src/core/api/favorite.ts
+++ sellpoint_frontend/src/core/api/favorite.ts
@@ -1,10 +1,20 @@
 import { FavoriteAd } from './../../models/favorite';
 import client from "../client";
+import { Ad } from "../../models/ad";
+import User from "../../models/user";
 
 class FavoriteAPI {
     async getAllFavorites(): Promise<FavoriteAd[]> {
         const response = await client.get(`favorite/list/`);
         return response.data;
+    }
+
+    async createFavorite(user: User, ad: Ad): Promise<FavoriteAd> {
+        const response = await client.post("favorite/create/", {
+            user: user,
+            ad: ad,
+          });
+    return response.data;
     }
 }
 
